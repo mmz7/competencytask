7891a1d018591c9aadad37dfd363514e6b285121
mmz7 <mmzhang711@gmail.com>
2022-10-11 11:40:33 -0400

    post get methods

--- app/src/main/java/com/example/competencytask/MainActivity.java
+++ app/src/main/java/com/example/competencytask/MainActivity.java
@@ -8,6 +8,20 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     RadioGroup rgroup;
@@ -32,8 +46,65 @@ public class MainActivity extends AppCompatActivity {
                 } else if(selectedid == R.id.loveo2o) {
                     movie = "Love O2O";
                 }
+                post(movie);
             }
         });
 
+    }
+
+    public void post(String movie) {
+        String url = "https://mzhang-resagain.sites.tjhsst.edu/apppost";
+        RequestQueue requestQ = Volley.newRequestQueue(this);
+
+        JSONObject data = new JSONObject();
+        try {
+            data.put("vote", movie);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, data, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                System.out.println(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        requestQ.add(jsonObjectRequest);
+    }
+    public void get() {
+        String url = "https://mzhang-resagain.sites.tjhsst.edu/appget";
+        List<String> jsonResponses = new ArrayList<>();
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("data");
+                    for(int i = 0; i < jsonArray.length(); i++){
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        String email = jsonObject.getString("email");
+
+                        jsonResponses.add(email);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
     }
 }
